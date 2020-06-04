158813da6ca9b7536987d1d05f4d32871cdf258f
tiwiz <roberto.orgiu@gmail.com>
2020-06-04 16:36:17 +0200

    Add other queries

--- app/src/main/java/io/rob/diet/storage/DietDao.kt
+++ app/src/main/java/io/rob/diet/storage/DietDao.kt
@@ -2,13 +2,18 @@ package io.rob.diet.storage
 
 import androidx.room.Dao
 import androidx.room.Query
-import io.rob.diet.meal.Meal
-import io.rob.diet.meal.ProteinPerDay
+import io.rob.diet.meal.*
 
 
 @Dao
 interface DietDao {
 
     @Query("SELECT * FROM protein WHERE meal = :meal AND day = :day")
-    suspend fun getProteinByMealAndDay(meal: Meal, day: Int) : ProteinPerDay
+    suspend fun getProteinByMealAndDay(meal: Meal, day: Int): ProteinPerDay
+
+    @Query("SELECT * FROM snack_portions WHERE meal = :meal")
+    suspend fun getSnackPortionsByMeal(meal: Meal): List<SnackPortion>
+
+    @Query("SELECT * FROM meal_portions WHERE type = :type")
+    suspend fun getFoodAlternativesByType(type: FoodType): List<MealPortion>
 }
