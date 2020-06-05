e57f0c4146cdf5c846481bdff1d2a38732b4b0c2
tiwiz <roberto.orgiu@gmail.com>
2020-06-05 11:38:48 +0200

    Add Hilt

--- app/src/main/java/io/rob/diet/DietApplication.kt
+++ app/src/main/java/io/rob/diet/DietApplication.kt
@@ -2,7 +2,9 @@ package io.rob.diet
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class DietApplication : Application() {
 
     override fun onCreate() {
--- app/src/main/java/io/rob/diet/MainActivity.kt
+++ app/src/main/java/io/rob/diet/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import io.rob.diet.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
