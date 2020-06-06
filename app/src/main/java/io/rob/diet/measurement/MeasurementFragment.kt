a3f4e4ed072aaf468a0271f51830f28f69ec6ef4
tiwiz <roberto.orgiu@gmail.com>
2020-06-06 10:54:41 +0200

    Create new Bottom Sheet Fragment to add new measurements

--- app/src/main/java/io/rob/diet/measurement/MeasurementFragment.kt
+++ app/src/main/java/io/rob/diet/measurement/MeasurementFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import io.rob.diet.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,7 +18,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MeasurementFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MeasurementFragment : Fragment() {
+class MeasurementFragment : BottomSheetDialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
