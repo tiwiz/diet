5b8c93f2feb6899cf1959c68e349d18127c0aaf9
tiwiz <roberto.orgiu@gmail.com>
2020-06-08 10:03:16 +0200

    Start LCE for progress

--- app/src/main/java/io/rob/diet/progress/ProgressFragment.kt
+++ app/src/main/java/io/rob/diet/progress/ProgressFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.rob.diet.R
+import io.rob.diet.common.Lce
 import io.rob.diet.common.updateExternalColors
 import io.rob.diet.databinding.FragmentProgressBinding
 
@@ -35,5 +37,36 @@ class ProgressFragment : Fragment() {
             val action = ProgressFragmentDirections.actionProgressFragmentToMeasurementFragment()
             findNavController().navigate(action)
         }
+
+        viewModel.recap.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is Lce.Loading -> onLoading()
+                is Lce.Success -> onSuccess(it.data)
+                else -> onError()
+            }
+        })
+    }
+
+    private fun onLoading() {
+        binding.loading.visibility = View.VISIBLE
+        binding.emptyProgress.visibility = View.GONE
+        binding.progressView.visibility = View.GONE
+    }
+
+    private fun onError() {
+        binding.loading.visibility = View.GONE
+        binding.emptyProgress.visibility = View.VISIBLE
+        binding.progressView.visibility = View.GONE
+    }
+
+    private fun onSuccess(action: RecapUI) {
+        binding.loading.visibility = View.GONE
+        binding.emptyProgress.visibility = View.GONE
+        binding.progressView.visibility = View.VISIBLE
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.fetchRecap()
     }
 }
