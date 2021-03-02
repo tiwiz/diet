2f55dc81a779bed3d953d674141138d6f401e349
rob <roberto.orgiu@gmail.com>
2021-03-02 09:23:26 +0100

    Remove view code

--- app/src/main/java/io/rob/diet/MainActivity.kt
+++ app/src/main/java/io/rob/diet/MainActivity.kt
@@ -9,12 +9,8 @@ import io.rob.diet.ui.theme.DietTheme
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
 
         setContent {
             DietTheme {
--- app/src/main/java/io/rob/diet/compose/LoadingUI.kt
+++ app/src/main/java/io/rob/diet/compose/LoadingUI.kt
@@ -2,12 +2,9 @@ package io.rob.diet.compose
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,14 +29,6 @@ fun LoadingUI() {
         LottieAnimation(
             animationSpec,
             modifier = Modifier.requiredSize(196.dp)
-        )
-        Text(
-            text = stringResource(id = R.string.loading),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp),
-            color = MaterialTheme.colors.primary,
-            style = MaterialTheme.typography.h1
         )
         DietTitle(titleRes = R.string.loading)
     }
