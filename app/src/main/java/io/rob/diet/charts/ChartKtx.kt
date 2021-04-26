8d58d43c67f9c61d002e46ef94e6ca93bac6f8d8
rob <roberto.orgiu@gmail.com>
2021-04-26 14:43:05 +0200

    Polish chart UI

---
+++ app/src/main/java/io/rob/diet/charts/ChartKtx.kt
@@ -0,0 +1,19 @@
+package io.rob.diet.charts
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.runtime.Composable
+
+fun Array<Float>.wrap(): List<Pair<ChartRowElement, ChartRowElement>> =
+    mapIndexed { index, value -> ChartRowElement(index, value) }.zipWithNext()
+
+@Composable
+fun Array<Float>.ChartRow(
+    action: @Composable RowScope.(left: ChartRowElement, right: ChartRowElement) -> Unit
+) {
+    wrap().forEach {
+        Row {
+            action(it.first, it.second)
+        }
+    }
+}
--- app/src/main/java/io/rob/diet/charts/ChartUi.kt
+++ app/src/main/java/io/rob/diet/charts/ChartUi.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -22,6 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import io.rob.diet.Charts
@@ -43,8 +45,6 @@ fun LineChart(
 ) {
     val pointColor = MaterialTheme.colors.primaryVariant
     val lineColor = MaterialTheme.colors.primary
-
-    val lastSelectedIndex = remember { mutableStateOf(-1) }
     val systemUiController = rememberSystemUiController()
 
     Column(
@@ -60,8 +60,8 @@ fun LineChart(
         Card(
             backgroundColor = MaterialTheme.colors.secondaryVariant,
             shape = RoundedCornerShape(
-                bottomEnd = 16.dp,
-                bottomStart = 16.dp
+                bottomEnd = 24.dp,
+                bottomStart = 24.dp
             ),
             elevation = 0.dp
         ) {
@@ -78,7 +78,6 @@ fun LineChart(
                     fontWeight = FontWeight.Bold
                 )
                 Chart(
-                    lastSelectedIndex = lastSelectedIndex,
                     points = points,
                     pointColor = pointColor,
                     lineColor = lineColor
@@ -89,18 +88,21 @@ fun LineChart(
         DietTitle(titleRes = R.string.history_title)
 
         Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-            points.toList()
-                .zipWithNext()
-                .forEachIndexed { index, value ->
-                    HistoryElement(
-                        index = index,
-                        lastSelectedIndex = lastSelectedIndex,
-                        descriptions = descriptions,
-                        value = value.first
-                    )
-                }
+            points.ChartRow { left, right ->
+                HistoryElement(
+                    index = left.index,
+                    descriptions = descriptions,
+                    value = left.value,
+                    modifier = Modifier.weight(1f)
+                )
 
-
+                HistoryElement(
+                    index = right.index,
+                    descriptions = descriptions,
+                    value = right.value,
+                    modifier = Modifier.weight(1f)
+                )
+            }
         }
     }
 }
@@ -108,36 +110,40 @@ fun LineChart(
 @Composable
 private fun HistoryElement(
     index: Int,
-    lastSelectedIndex: MutableState<Int>,
     descriptions: Array<String>,
-    value: Float
+    value: Float,
+    modifier: Modifier
 ) {
-    var backModifier = Modifier
-        .fillMaxWidth()
-    var textColor = MaterialTheme.typography.body1.color
+    val textColor = MaterialTheme.typography.body1.color
 
-    if (index == lastSelectedIndex.value) {
-        backModifier = backModifier.background(MaterialTheme.colors.primary)
-        textColor = MaterialTheme.colors.background
-    }
-
-    Box(modifier = backModifier) {
-        Text(
-            text = "${descriptions[index]}: $value",
-            modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp),
-            color = textColor
-        )
+    Card(
+        modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+        elevation = 4.dp
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(vertical = 8.dp)
+        ) {
+            Text(
+                text = descriptions[index],
+                fontSize = 10.sp,
+                color = textColor
+            )
+            Text(
+                text = "$value",
+                fontSize = 18.sp,
+                color = textColor
+            )
+        }
     }
 }
 
 @Composable
 private fun Chart(
-    lastSelectedIndex: MutableState<Int>,
     points: Array<Float>,
     pointColor: Color,
     lineColor: Color
 ) {
-    val lastTap: MutableState<Float?> = remember { mutableStateOf(null) }
 
     Card(
         elevation = 4.dp,
@@ -147,28 +153,14 @@ private fun Chart(
         Canvas(modifier = Modifier
             .fillMaxWidth()
             .height(200.dp)
-            .padding(16.dp)
-            .pointerInteropFilter {
-                when (it.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        lastTap.value = it.x
-                        true
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        lastTap.value = null
-                        lastSelectedIndex.value = -1
-                        true
-                    }
-                    else -> false
-                }
-            },
+            .padding(16.dp),
             onDraw = {
 
                 val width = size.width
                 val height = size.height
 
                 val verticalFactor = height / points.maxOrNull()!!
-                val horizontalFactor = width / points.size
+                val horizontalFactor = width / (points.size - 1)
 
                 points.forEachIndexed { hStep, value ->
                     drawCircle(
@@ -197,47 +189,6 @@ private fun Chart(
                             strokeWidth = 5f
                         )
                     }
-                }
-
-                lastTap.value?.let { x ->
-
-                    val column = x / horizontalFactor
-                    val lowerBound = truncate(column).toInt()
-                    val upperBound = column.roundToInt()
-
-                    /**
-                     * Calculate index based on the closest bound.
-                     * if the space between the touch point and the upper bound is smaller than
-                     * the space between the touch point and the lower bound AND the upper bound does
-                     * not exceed the number of items, than it will choose the upper bound, otherwise
-                     * it will choose the lower one
-                     */
-                    val index =
-                        if ((upperBound.toFloat() - column) < (column - lowerBound)
-                            && upperBound < points.size
-                        ) {
-                            upperBound
-                        } else {
-                            lowerBound
-                        }
-
-                    lastSelectedIndex.value = index
-
-                    /**
-                     * Draw a rectangle column to highlight the value
-                     */
-
-                    val startingX = (index * horizontalFactor) - (horizontalFactor / 2)
-
-                    val topLeft = Offset(x = startingX, y = 0f)
-                    val size = Size(width = horizontalFactor, height = height)
-
-                    drawRect(
-                        color = pointColor,
-                        topLeft = topLeft,
-                        size = size,
-                        alpha = .5f
-                    )
                 }
             })
     }
